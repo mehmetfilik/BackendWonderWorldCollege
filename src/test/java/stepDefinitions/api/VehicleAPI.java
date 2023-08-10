31f377994f6b94a3565fd7376ebdbcc06027936c
MUSTAFA ÖZSAMUR <mustafaozsamur22@gmail.com>
2023-08-10 23:00:54 +0200

    mustafa 10.08/V1

---
+++ src/test/java/stepDefinitions/api/VehicleAPI.java
@@ -0,0 +1,84 @@
+package stepDefinitions.api;
+
+import hooks.api.HooksAPI;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.json.JSONObject;
+import org.junit.Assert;
+
+import java.util.Arrays;
+
+import static hooks.api.HooksAPI.spec;
+import static io.restassured.RestAssured.given;
+
+public class VehicleAPI {
+    String fullPath;
+
+    Response response;
+
+    JSONObject reqBody;
+
+    @Given("User sets {string} path param")
+    public void user_sets_path_param(String rawPaths) {
+        String [] paths = rawPaths.split("/");
+
+        System.out.println(Arrays.toString(paths));
+
+
+        StringBuilder tempPath = new StringBuilder("/{");
+
+
+        for (int i = 0; i < paths.length; i++) {
+
+            String key = "pp" + i;
+            String value = paths[i].trim();
+
+            HooksAPI.spec.pathParam(key,value);
+
+            tempPath.append(key + "}/{");
+        }
+        tempPath.deleteCharAt(tempPath.lastIndexOf("{"));
+        tempPath.deleteCharAt(tempPath.lastIndexOf("/"));
+
+        fullPath = tempPath.toString(); // {pp1}/{pp2}/{pp3}
+        System.out.println("fullPath = " + fullPath);
+    }
+    @Given("For {string} is sent Get request")
+    public void for_is_sent_get_request(String string) {
+        response = given()
+                .spec(spec)
+                .contentType(ContentType.JSON)
+                .headers("Authorization","Bearer " + HooksAPI.token)
+                .when()
+                .get(fullPath);
+
+        response.prettyPrint();
+    }
+    @Given("Verifies that the status code in Response is {int}.")
+    public void verifies_that_the_status_code_in_response_is(Integer statusCode) {
+     response.then().assertThat().statusCode(statusCode);
+    }
+    @Then("Verifies that {string} is {string} in Response")
+    public void verifies_that_is_in_response(String bodyName, String value) {
+        JsonPath responseJP=response.jsonPath();
+
+        Assert.assertEquals(value,responseJP.get(bodyName));
+    }
+
+    @When("A GET request is sent with invalid authorization")
+    public void aGETRequestIsSentWithInvalidAuthorization() {
+        response = given()
+                .spec(spec)
+                .contentType(ContentType.JSON)
+                .headers("Authorization","Bearer " ,"fEkdQhAOQGONpsOfG6etV3ojhjhUoC")
+                .when()
+                .get(fullPath);
+
+        response.prettyPrint();
+    }
+}
--- src/test/resources/features/api/mustafa/[US_13].feature
+++ src/test/resources/features/api/mustafa/[US_13].feature
@@ -1,6 +1,16 @@
 Feature: [US_13]As an administrator, I want to access the Vehicle List through API connection.
-  @mustafa
+
+
   Scenario:[US_13-->TC_01] Using valid authorization for GET request to api/vehicleList should return status 200 with 'Success' message in response
 
-    Given User sets "api/vehicleList" path param.
-    And For "vehicleList" is sent Get request.
+    Given User sets "api/vehicleList" path param
+    And For "vehicleList" is sent Get request
+    And Verifies that the status code in Response is 200.
+    Then Verifies that "message" is "Success" in Response
+
+  @mustafa
+  Scenario: Sending a GET request with invalid authorization to api/vehicleList
+    Given User sets "api/vehicleList" path param
+    When A GET request is sent with invalid authorization
+    And Verifies that the status code in Response is 403.
+    Then Verifies that "message" is "failed" in Response
