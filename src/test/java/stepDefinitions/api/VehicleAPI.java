58e1b5c353b3f876f6efb20c497801a416f42feb
SametYigit <samettyigitt0@gmail.com>
2023-08-15 20:40:20 +0300

    samet 15.08/v2

--- src/test/java/stepDefinitions/api/VehicleAPI.java
+++ src/test/java/stepDefinitions/api/VehicleAPI.java
@@ -349,16 +349,16 @@ public class VehicleAPI {
                 // .contentType(ContentType.JSON)
                 .headers("Authorization", "Bearer " + HooksAPI.invalidToken)
                 .when()
-<<<<<<< HEAD
+
                 .get("/{pp1}/{pp2}");
 
 
             responseJP=response.jsonPath();
-=======
-                .get(CommonAPI.fullPath);
 
-        responseJP = response.jsonPath();
->>>>>>> main
+
+
+
+
 
       //  response.then().assertThat().statusCode(403).body("message", Matchers.equalTo("failed"));
 
@@ -569,56 +569,9 @@ public class VehicleAPI {
     }
 
 
-    @Then("send endpoint valid PATCH body and verify that the returned status code is {int} and the {string} information is {string}")
-    public void sendEndpointValidPATCHBodyAndVerifyThatTheReturnedStatusCodeIsAndTheInformationIs(int arg0, String arg1, String arg2) {
 
 
-        requestBody = new JSONObject();
-        requestBody.put("id",28);
-        requestBody.put("vehicle_no", "TH2023");
-        requestBody.put("vehicle_model","Mercedes VITO");
-        requestBody.put("vehicle_photo", "7584709375093705973097490479895!fd.png");
-        requestBody.put("manufacture_year", "2023");
-        requestBody.put("registration_number", "KMTT-957845");
-        requestBody.put("chasis_number", "10643");
-        requestBody.put("max_seating_capacity", "30");
-        requestBody.put("driver_name", "Ahmet Enhakikiöz");
-        requestBody.put("driver_licence", "T74879489");
-        requestBody.put("driver_contact", "94578849850");
-        requestBody.put("note", "");
 
-        response=given().spec(spec).contentType(ContentType.JSON) .headers("Authorization", "Bearer " + HooksAPI.token)
-                .when().body(requestBody.toString())
-                .patch(CommonAPI.fullPath);
-
-        response.then().assertThat().statusCode(200).body("message",Matchers.equalTo("Success"));
-
-    }
-
-    @Then("send endpoint invalid PATCH body and verify that the returned status code is {int} and the {string} information is {string}")
-    public void sendEndpointInvalidPATCHBodyAndVerifyThatTheReturnedStatusCodeIsAndTheInformationIs(int arg0, String arg1, String arg2) {
-        requestBody = new JSONObject();
-        requestBody.put("id",28);
-        requestBody.put("vehicle_no", "TH2023");
-        requestBody.put("vehicle_model","Mercedes VITO");
-        requestBody.put("vehicle_photo", "7584709375093705973097490479895!fd.png");
-        requestBody.put("manufacture_year", "2023");
-        requestBody.put("registration_number", "KMTT-957845");
-        requestBody.put("chasis_number", "10643");
-        requestBody.put("max_seating_capacity", "30");
-        requestBody.put("driver_name", "Ahmet Enhakikiöz");
-        requestBody.put("driver_licence", "T74879489");
-        requestBody.put("driver_contact", "94578849850");
-        requestBody.put("note", "");
-
-        response=given()
-                .spec(spec).contentType(ContentType.JSON)
-                .headers("Authorization", "Bearer " + HooksAPI.invalidToken)
-                .when().body(requestBody.toString())
-                .patch(CommonAPI.fullPath);
-
-        response.then().assertThat().statusCode(403).body("message",Matchers.equalTo("failed"));
-    }
 
     @Then("send endpoint valid DELETE body and verify that the returned status code is {int} and the {string} information is {string}")
     public void sendEndpointValidDELETEBodyAndVerifyThatTheReturnedStatusCodeIsAndTheInformationIs(int statusCode, String bodyName, String value) {
