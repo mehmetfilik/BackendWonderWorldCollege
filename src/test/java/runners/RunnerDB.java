831868da09d8e320f46db4848c8a5863e840822d
mehmetfilik <filikmehmet02gmail.com>
2023-08-17 20:35:10 +0200

    filik 17.08/v2

--- src/test/java/runners/RunnerDB.java
+++ src/test/java/runners/RunnerDB.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
 
         features = "src/test/resources/features",
         glue = {"stepDefinitions", "hooks"},
-        tags = "@sam",
+        tags = "@db",
 
         dryRun = false
 
